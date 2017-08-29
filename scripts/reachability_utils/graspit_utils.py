d80f3b8243f8e4e6425fd2ddf3237511ecfcf577
iretiayo <iretibob@gmail.com>
2017-08-29 13:33:40 -0400

    added code to evaluate simAnnGrasps by closing the hand and computing the quality/energy

--- scripts/reachability_utils/graspit_utils.py
+++ scripts/reachability_utils/graspit_utils.py
@@ -2,6 +2,7 @@
 
 import os
 import time
+import copy
 import subprocess
 from collections import namedtuple
 
@@ -12,6 +13,37 @@ import graspit_commander
 import grid_sample_client
 
 GraspEnergies = namedtuple('GraspEnergies', ['grasps', 'energies'], verbose=False)
+
+def evaluateSimAnnGrasps(pre_grasps):
+	import graspit_commander
+	gc = graspit_commander.GraspitCommander()
+
+	grasps = []
+	for i, pre_grasp in enumerate(pre_grasps):
+		gc.toggleAllCollisions(False)
+		gc.setRobotPose(pre_grasp.pose)
+		gc.forceRobotDof(pre_grasp.dofs)
+		gc.toggleAllCollisions(True)
+
+		gc.autoGrasp()
+		robot_state = gc.getRobot(0)
+
+		try:
+			quality = gc.computeQuality()
+			volume_quality = quality.volume
+			epsilon_quality = quality.epsilon
+		except:
+			volume_quality = -1
+			epsilon_quality = -1
+
+		grasp = copy.deepcopy(pre_grasp)
+		grasp.pose = robot_state.robot.pose
+		grasp.volume_quality = volume_quality
+		grasp.epsilon_quality = epsilon_quality
+		grasp.dofs = robot_state.robot.dofs
+		grasps.append(grasp)
+	return grasps
+
 
 def get_grasp_from_graspit_sim_ann(
 	mesh_filepath,
@@ -27,6 +59,11 @@ def get_grasp_from_graspit_sim_ann(
 	gc.importGraspableBody(mesh_filepath)
 
 	result = gc.planGrasps(search_energy=search_energy, max_steps=max_steps)
+
+	# close hand and evaluate grasps
+	pre_grasps = result.grasps
+	grasps = evaluateSimAnnGrasps(pre_grasps)
+	result.grasps = grasps
 
 	return result
 
@@ -44,7 +81,7 @@ def get_grasp_from_graspit_ellipse(
 	gc.importGraspableBody(mesh_filepath)
 
 	gl = grid_sample_client.GridSampleClient()
-	result = gl.computePreGrasps(10, 0)
+	result = gl.computePreGrasps(20, 0)
 	pre_grasps = result.grasps
 	grasps = gl.evaluatePreGrasps(pre_grasps, pre_grasp_dofs=pre_grasp_dofs)
 
@@ -85,7 +122,7 @@ def evaluate_grasp_list(
 		grasp_energy = gc.computeEnergy(search_energy)
 		energies.append(grasp_energy.energy)
 
-	energies, grasps = zip(*sorted(zip(energies, grasps)))
+	# energies, grasps = zip(*sorted(zip(energies, grasps)))
 
 	grasp_results = GraspEnergies(grasps=grasps, energies=energies)
 	return grasp_results
